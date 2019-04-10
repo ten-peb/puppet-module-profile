598bc1d62e4388b0b35b95228cfd509b4fe67b7f
Peter L. Berghold <pberghold@tenna.com>
2019-04-10 18:36:41 +0000

    initial commit, nothing here to see (yet)

---
+++ manifests/base/common.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile::base::common
+class profile::base::common {
+}
