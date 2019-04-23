b02a40dec6d4c9afe7019ad75003221d990e175c
Peter L. Berghold <pberghold@tenna.com>
2019-04-23 16:38:43 +0000

    Added docker management

---
+++ manifests/docker/host.pp
@@ -0,0 +1,9 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile::docker::host
+class profile::docker::host {
+  include docker
+}
