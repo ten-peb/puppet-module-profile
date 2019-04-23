d665f5d7d31672ed12929a66aac810681dbedf90
Peter L. Berghold <pberghold@tenna.com>
2019-04-23 16:26:04 +0000

    Moved jenkins related code into its own module.

--- manifests/jenkins/master.pp
+++ manifests/jenkins/master.pp
@@ -8,27 +8,6 @@ class profile::jenkins::master {
   include tenna::users::peter
   include tenna::users::jenkins
 
-  file{'/etc/apt/sources.list.d/jenkins.list':
-    ensure  => present,
-    content => template('profile/jenkins/jenkins.list.erb'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644'
-  }
-  exec{'refresh aptcache for jenkins':
-    command => 'apt-cache update',
-    path    => '/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin',
-    require => File['/etc/apt/sources.list.d/jenkins.list'],
-  }
-  package{'jenkins':
-    ensure  => latest,
-    require => Exec['refresh aptcache for jenkins'],
-    notify  => Service['jenkins']
-  }
-
-  service{'jenkins':
-    ensure  => running,
-    enable  => true,
-    require => Package['jenkins']
-  }
+  include jenkins
+  
 }
